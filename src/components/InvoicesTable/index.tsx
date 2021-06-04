b7cb9546f261bef95702d9d792ea574b5e985236
Stenio Oliveira <steniooliv@gmail.com>
2021-06-04 17:34:37 -0300

    create invoices table component

---
+++ src/components/InvoicesTable/index.tsx
@@ -0,0 +1,63 @@
+import downloadImg from '../../assets/download.svg';
+import pdfImg from '../../assets/pdf.svg';
+
+import { Container, Content } from './styles'
+
+export function InvoicesTable() {
+
+  return (
+    <Container>
+      <h1>NOTAS FISCAIS DE CLIENTE DA CONTABILIDADE</h1>
+
+      <Content>
+        <table>
+          <thead>
+            <tr>
+              <th>Número</th>
+              <th>Série</th>
+              <th>Valor Total</th>
+              <th>Cliente</th>
+              <th><img src={downloadImg} alt="Download" /></th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>0015452</td>
+              <td>001</td>
+              <td>1.6549,00</td>
+              <td>Stenio de Oliveira</td>
+              <td><a href="#"><img src={pdfImg} alt="PDF" /></a></td>
+            </tr>
+
+            <tr>
+              <td>0015452</td>
+              <td>001</td>
+              <td>1.6549,00</td>
+              <td>Stenio de Oliveira</td>
+              <td><a href="#"><img src={pdfImg} alt="PDF" /></a></td>
+            </tr>
+
+            <tr>
+              <td>0015452</td>
+              <td>001</td>
+              <td>1.6549,00</td>
+              <td>Stenio de Oliveira</td>
+              <td><a href="#"><img src={pdfImg} alt="PDF" /></a></td>
+            </tr>
+
+            <tr>
+              <td>0015452</td>
+              <td>001</td>
+              <td>1.6549,00</td>
+              <td>Stenio de Oliveira</td>
+              <td><a href="#"><img src={pdfImg} alt="PDF" /></a></td>
+            </tr>
+            
+          </tbody>
+        </table>
+      </Content>
+
+    </Container>
+  );
+}
