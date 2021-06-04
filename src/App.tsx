c1d85f4080f5ece128c4960f02f514acbaaf4507
Stenio Oliveira <steniooliv@gmail.com>
2021-06-04 17:35:07 -0300

    change started components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import { Footer } from "./components/Footer";
+import { HeaderLogged } from "./components/HeaderLogged";
 import { Header } from "./components/Header";
 import { Login } from "./components/Login";
 import { GlobalStyles } from "./styles/global";
+import { CustomersList } from "./components/CustomersList";
+import { InvoicesTable } from "./components/InvoicesTable";
 
 function App() {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
 
-      <Login />
+      <HeaderLogged />
+
+      {/* <Login /> */}
+
+      {/* <CustomersList /> */}
+
+      <InvoicesTable />
 
       <Footer />
 
