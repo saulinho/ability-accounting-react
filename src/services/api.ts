7fffc2caf091221e920dd0724f569d8c654de46b
Stenio Oliveira <steniooliv@gmail.com>
2021-06-09 09:08:40 -0300

    add local and enternal api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const api =  axios.create({
+  // baseURL: "http://172.21.190.242:3001/api",
   baseURL: "https://ability-accounting-api.herokuapp.com/api",
   withCredentials: true,
 });
