837889d7bd21afe1206d7426b90530682ae49446
Stenio Oliveira <steniooliv@gmail.com>
2021-06-14 17:57:46 -0300

    create invoice type page

---
+++ src/pages/Invoices.tsx
@@ -0,0 +1,22 @@
+import { Footer } from "../components/Footer";
+import { HeaderLogged } from "../components/HeaderLogged";
+import { InvoiceType } from "../components/InvoiceType";
+
+interface InvoicesProps {
+  user: {} | {
+    name: string,
+    email: string,
+    accounting_id: number
+  }
+  handleLogout: () => void
+}
+
+export function Invoices(props: InvoicesProps) {
+  return (
+    <>
+      <HeaderLogged user={props.user} handleLogout={props.handleLogout}/>
+      <InvoiceType />
+      <Footer />
+    </>
+  );
+}
