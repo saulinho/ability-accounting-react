98adf613c21d8d8c3378f90c60ce50db5cc418a5
Stenio Oliveira <steniooliv@gmail.com>
2021-06-25 14:42:47 -0300

    change api url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api =  axios.create({
-  baseURL: "http://172.31.235.101:3001/api",
-  // baseURL: "https://ability-accounting-api.herokuapp.com/api",
+  // baseURL: "http://172.31.227.82:3001/api",
+  baseURL: "https://ability-accounting-api.herokuapp.com/api",
   withCredentials: true,
 });
