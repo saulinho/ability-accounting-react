17f3dff94f4f758e22c04060e3026db92f471e9e
Saulo Morais Lara <saulinhomorais@gmail.com>
2021-12-08 18:52:51 -0300

    alter endpoint of backend

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export const api =  axios.create({
   // baseURL: "http://172.31.227.82:3001/api",
-  baseURL: "https://ability-accounting-api.herokuapp.com/api",
+  baseURL: "https://ability-accounting-back.herokuapp.com/api",
   withCredentials: true,
 });
